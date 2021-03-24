e1010dfc6e08b7011b6cb45b6ee580b68f758549
Arina Vernidub <avernidub@ourgapps.com>
2021-03-24 08:56:40 +0300

    update offers test

--- src/store/offers/offers.test.js
+++ src/store/offers/offers.test.js
@@ -4,77 +4,78 @@ import {offers} from './offers';
 import {ActionType} from '../action';
 import {INIT_CITY} from '../../const';
 import {fetchOffersList} from '../api-actions';
-import {APIRoute} from '../../const';
+import {APIRoute, sortType} from '../../const';
 
 const api = createAPI(() => {});
-const offersList = [{
-  "bedrooms": 3,
-  "city": {
-    "location": {
-      "latitude": 52.370216,
-      "longitude": 4.895168,
-      "zoom": 10
+const offersList = [
+  {
+    "bedrooms": 3,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": INIT_CITY
     },
-    "name": `Amsterdam`
-  },
-  "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
-  "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
-  "host": {
-    "avatar_url": `img/1.png`,
-    "id": 3,
-    "is_pro": true,
-    "name": `Angelina`
-  },
-  "id": 1,
-  "images": [`img/1.png`, `img/2.png`],
-  "is_favorite": false,
-  "is_premium": false,
-  "location": {
-    "latitude": 52.35514938496378,
-    "longitude": 4.673877537499948,
-    "zoom": 8
-  },
-  "max_adults": 4,
-  "preview_image": `img/1.png`,
-  "price": 120,
-  "rating": 4.8,
-  "title": `Beautiful & luxurious studio at great location`,
-  "type": `apartment`
-},
-{
-  "bedrooms": 3,
-  "city": {
-    "location": {
-      "latitude": 52.370216,
-      "longitude": 4.895168,
-      "zoom": 10
+    "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+    "host": {
+      "avatar_url": `img/1.png`,
+      "id": 3,
+      "is_pro": true,
+      "name": `Angelina`
     },
-    "name": `Paris`
+    "id": 1,
+    "images": [`img/1.png`, `img/2.png`],
+    "is_favorite": false,
+    "is_premium": false,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "max_adults": 4,
+    "preview_image": `img/1.png`,
+    "price": 120,
+    "rating": 4.8,
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `apartment`
   },
-  "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
-  "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
-  "host": {
-    "avatar_url": `img/1.png`,
-    "id": 3,
-    "is_pro": true,
-    "name": `Angelina`
-  },
-  "id": 1,
-  "images": [`img/1.png`, `img/2.png`],
-  "is_favorite": false,
-  "is_premium": false,
-  "location": {
-    "latitude": 52.35514938496378,
-    "longitude": 4.673877537499948,
-    "zoom": 8
-  },
-  "max_adults": 4,
-  "preview_image": `img/1.png`,
-  "price": 120,
-  "rating": 4.8,
-  "title": `Beautiful & luxurious studio at great location`,
-  "type": `apartment`
-}];
+  {
+    "bedrooms": 3,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": `Paris`
+    },
+    "description": `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+    "goods": [`Heating`, `Kitchen`, `Cable TV`, `Washing machine`, `Coffee machine`, `Dishwasher`],
+    "host": {
+      "avatar_url": `img/1.png`,
+      "id": 3,
+      "is_pro": true,
+      "name": `Angelina`
+    },
+    "id": 2,
+    "images": [`img/1.png`, `img/2.png`],
+    "is_favorite": false,
+    "is_premium": false,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "max_adults": 4,
+    "preview_image": `img/1.png`,
+    "price": 120,
+    "rating": 4.8,
+    "title": `Beautiful & luxurious studio at great location`,
+    "type": `apartment`
+  }];
 
 describe(`Reducer 'offers' should work correctly`, () => {
   it(`Reducer without additional parameters should return initial state`, () => {
@@ -87,7 +88,7 @@ describe(`Reducer 'offers' should work correctly`, () => {
   });
 
   it(`Reducer should update offers by load offers`, () => {
-    const state = {offers: {}, isDataLoaded: false, city: INIT_CITY};
+    const state = {offers: [], isDataLoaded: false, city: INIT_CITY};
     const loadOffersAction = {
       type: ActionType.LOAD_OFFERS,
       payload: offersList
@@ -98,14 +99,35 @@ describe(`Reducer 'offers' should work correctly`, () => {
   });
 
   it(`Reducer should update city by change city`, () => {
-    const state = {offers: {}, isDataLoaded: false, city: INIT_CITY};
+    const state = {offers: [], isDataLoaded: false, city: INIT_CITY};
     const updateCityAction = {
       type: ActionType.CHANGE_CITY,
       payload: `Test`
     };
 
     expect(offers(state, updateCityAction))
-      .toEqual({offers: {}, isDataLoaded: false, city: `Test`});
+      .toEqual({offers: [], isDataLoaded: false, city: `Test`});
+  });
+
+  it(`Reducer should filter offer by city`, () => {
+    const state = {offers: [], isDataLoaded: true, city: INIT_CITY};
+    const filterOffersAction = {
+      type: ActionType.FILTER_OFFERS,
+    };
+
+    expect(offers(state, filterOffersAction))
+      .toEqual({offers: [], isDataLoaded: true, city: INIT_CITY});
+  });
+
+  it(`Reducer should sort offer by type`, () => {
+    const state = {offers: [], isDataLoaded: true, city: INIT_CITY};
+    const sortOffersAction = {
+      type: ActionType.SORT_OFFERS,
+      payload: sortType.ACS_PRICE
+    };
+
+    expect(offers(state, sortOffersAction))
+      .toEqual({offers: [], isDataLoaded: true, city: INIT_CITY});
   });
 });
 
