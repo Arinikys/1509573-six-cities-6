8ebc85ebfdcc5460a5d5d428cd83a3dfb386956a
Arina Vernidub <avernidub@ourgapps.com>
2021-03-28 19:48:10 +0300

    remove usestate from fvotites page

--- src/components/favorites-page/favorites-page.jsx
+++ src/components/favorites-page/favorites-page.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import CardsList from './cards-list';
 import Header from "../common/header/header";
